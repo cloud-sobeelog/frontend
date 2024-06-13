7cbb19913fc52c0e8d6cee2e9893a763433ad180
choiyoorim <yulim3839@naver.com>
2024-06-14 06:41:24 +0900

    refactor: 🔨서버 연결 코드 수정 필요

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-    baseURL: 'http://www.sobeelog.site',
+    baseURL: 'http://34.64.116.29',
     headers:{
         'content-Type': 'application/json',
     }
